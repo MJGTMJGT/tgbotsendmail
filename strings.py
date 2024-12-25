0c7d317db7c3ca159761bf55da4e178fc7fb9eb0
EvgenyM <matematik@mail.ru>
2024-12-25 22:34:32 +0900

    Update strings.py
    Добавил несколько сток для проверки вводимых данных

--- strings.py
+++ strings.py
@@ -1,4 +1,3 @@
-from config import SMTP_USER1, SMTP_USER2
 
 # Сообщения для команд
 INFO_MESSAGE = (
@@ -37,70 +36,71 @@ TOP_SECRET_INFO_TEMPLATE = (
 )
 
 CHANGE_EMAIL_PROMPT = "Укажите e-mail, на который будут пересылаться файлы:"
+
 EMAIL_UPDATED_MESSAGE = "Готово! e-mail получателя изменён на: "
 
 CHANGE_SUBJECT_PROMPT = "Укажите тему сообщения:"
+
 SUBJECT_UPDATED_MESSAGE = "Готово! Тема сообщения теперь: "
 
-CHANGE_SMTP_PROMPT = (
-    "Введите SMTP-сервер (1 - использовать smtp mail.ru, 2 - использовать smtp.gmail.com, или укажите свой сервер):"
-)
+CHANGE_SMTP_PROMPT = "Введите SMTP-сервер (1 - использовать smtp mail.ru, 2 - использовать smtp.gmail.com, или укажите свой сервер):"
 
-CHANGE_SMTP_SERVER_PROMPT = (
-    "Отправьте свой SMTP-сервер (например, smtp.mail.ru или smtp.gmail.com):"
-)
+CHANGE_SMTP_SERVER_PROMPT = "Отправьте свой SMTP-сервер (например, smtp.mail.ru или smtp.gmail.com):"
+
+CHANGE_SMTP_PROMPT_PASS = "Отправьте свой SMTP-сервер (например, smtp.mail.ru или smtp.gmail.com) или отправьте пароль для выбора SMTP-сервера по умолчанию:"
 
 SMTP_SERVER_UPDATED_MESSAGE = "Готово! Сервер изменён на: "
 
-CHANGE_SMTP_PORT_PROMPT = (
-    "Укажите порт SMTP-сервера (например, 465 для mail.ru или 587 для gmail.com). Текущее значение: "
-)
+CHANGE_SMTP_PORT_PROMPT = "Укажите порт SMTP-сервера (например, 465 для mail.ru или 587 для gmail.com). Текущее значение: "
 
-CHANGE_SMTP_PORT_PROMPT2 = (
-    "Введите порт SMTP-сервера (например, 465 для mail.ru или 587 для gmail.com):"
-)
+CHANGE_SMTP_PORT_PROMPT2 = "Введите порт SMTP-сервера (например, 465 для mail.ru или 587 для gmail.com):"
 
 SMTP_PORT_UPDATED_MESSAGE = "Готово! Порт изменён на: "
 
-CHANGE_SMTP_USER_PROMPT = (
-    "Укажите имя пользователя SMTP-сервера (например, user@mail.ru или user@gmail.com). Текущее значение: "
-)
+CHANGE_SMTP_USER_PROMPT = "Укажите имя пользователя SMTP-сервера (например, user@mail.ru или user@gmail.com). Текущее значение: "
 
-CHANGE_SMTP_USER_MESSAGE = (
-    "Введите логин SMTP-пользователя (например: user@gmail.com):"
-)
+CHANGE_SMTP_USER_MESSAGE = "Введите логин SMTP-пользователя (например: user@gmail.com):"
 
 SMTP_USER_UPDATED_MESSAGE = "Готово! Имя пользователя изменено на: "
 
-CHANGE_SMTP_PASSWORD_PROMPT = (
-    "Укажите пароль SMTP сервера, [для gmail.com и mail.ru можно получить в настройках безопасности (Пароль для приложений)]:"
-)
+CHANGE_SMTP_PASSWORD_PROMPT = "Укажите пароль SMTP сервера, [для gmail.com и mail.ru можно получить в настройках безопасности (Пароль для приложений)]:"
+
 SMTP_PASSWORD_UPDATED_MESSAGE = "Готово! Пароль SMTP-сервера изменён."
 
 START_MESSAGE = "Добро пожаловать! Укажите e-mail для пересылки файлов:"
-EMAIL_PROMPT = "Введите тему сообщения:"
-SMTP_SETTINGS_DEFAULT_MAILRU = (
-    "Настройки SMTP-сервера установлены по умолчанию (" +
-    SMTP_USER1 +
-    "). Можно отправлять файлы!"
-)
-SMTP_SETTINGS_DEFAULT_GMAIL = (
-    "Настройки SMTP-сервера установлены по умолчанию (" +
-    SMTP_USER2 +
-    "). Можно отправлять файлы!"
-)
 
-FILE_SENT_MESSAGE = "Файл отправлен на указанный e-mail и удалён с сервера."
-IMAGE_SENT_MESSAGE = "Изображение отправлено на указанный e-mail и удалено с сервера."
+EMAIL_PROMPT = "Введите тему сообщения:"
+
+SMTP_SETTINGS_DEFAULT_MAILRU = "Настройки SMTP-сервера установлены по умолчанию (sender@mail.ru). Можно отправлять файлы!"
+
+SMTP_SETTINGS_DEFAULT_GMAIL = "Настройки SMTP-сервера установлены по умолчанию (sender@gmail.com). Можно отправлять файлы!"
+
+FILE_SENT_MESSAGE = "Файл успешно отправлен на указанный e-mail!"
+
+IMAGE_SENT_MESSAGE = "Изображение успешно отправлено на указанный e-mail!"
+
+VIDEO_SENT_MESSAGE = "Видео успешно отправлено на указанный e-mail!"
+
 REGISTRATION_COMPLETE_MESSAGE = "Регистрация завершена! Теперь вы можете отправлять файлы."
+
 REGISTRATION_INCOMPLETE_MESSAGE = "Пожалуйста, завершите регистрацию с помощью команды /start."
 
-EMAIL_VALIDATION_ERROR = "Указан некорректный e-mail. Попробуйте снова."
 SMTP_PORT_ERROR = "Какой-то другой порт"
+
 EMAIL_SEND_SUCCESS_465 = "Сообщение успешно отправлено: 465"
+
 EMAIL_SEND_SUCCESS_587 = "Сообщение успешно отправлено: 587"
+
 EMAIL_SEND_ERROR = "Ошибка отправки email: {error}"
 
+EMAIL_VALIDATION_ERROR = "Указан некорректный e-mail. Попробуйте снова."
+
 NOT_SUPPORT_PORT = "Неподдерживаемый порт SMTP."
+
+VALIDATION_SMTP_REQUEST = "То, что вы отправили не является smtp-сервером!"
+
+VALIDATION_MAIL_REQUEST = "То, что вы отправили не является e-mail!"
+
+VALIDATION_PORT_REQUEST = "То, что вы отправили не является портом, должно быть число!"
 
 
