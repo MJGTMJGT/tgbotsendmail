f55f67f0a516ebceb43812b475b5a6bb5951d5b8
EvgenyM <matematik@mail.ru>
2024-12-29 00:49:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from handlers import register_handlers
 from database import init_db
 
 # Инициализация бота (Настройки бота)
-# bot = TeleBot.TeleBot(TOKEN)
 bot = TeleBot(TOKEN)
 
 # Инициализация базы данных
